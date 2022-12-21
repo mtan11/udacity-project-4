c4cfdb02dbcdf3614b408cea8825491b9b89f02c
Tuong An <mai.an@getcerebral.com>
2022-12-22 00:14:31 +0700

    add dockerfile

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=mtan11/ml-training:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-project4 --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacity-project4 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=mtan11/ml-training:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login
+docker tag ml-training:latest $DOCKER_ID_USER/ml-training:latest
+docker push $DOCKER_ID_USER/ml-training:latest
 # Step 3:
 # Push image to a docker repository
+docker push mtan11/ml-training:latest
